c190b42616e0ba0883aca4c9069b46b25b673a93
ParkourNinjaKitty <68483205+ParkourNinjaKitty@users.noreply.github.com>
2022-06-18 16:49:55 -0700

    Version 1.3.0
    Recolored the whole website
    Changed pet button and upgrade button from on click to event listeners
    Added effect when clicking the pet button that spawns meow or purr text
    Pictures are randomized past the final picture

--- petgnocchi.com-main/main.js
+++ petgnocchi.com-main/main.js
@@ -1,5 +1,5 @@
 /*Created by Camden Martin*/
-let currentVersion = 1.2
+let currentVersion = 1.3
 let previouslyOpenedVersion = parseInt(localStorage['version']) || currentVersion;
 let pets = parseInt(localStorage['pets']) || 0;
 let multcost = parseInt(localStorage['multcost']) || 100;
@@ -9,13 +9,17 @@ let totalImages = 14;
 function ready() {
     update_version();
     update_html();
+    document.getElementById("pet-button").addEventListener('click', clicked);
+    document.getElementById("upgrade-button").addEventListener('click', upgrade);
 }
 
-function clicked() {
+function clicked(e) {
     pets += multiplier;
     document.getElementById("amount").innerHTML = pets;
     localStorage['pets'] = pets;
+    purr_effect(e.pageX, e.pageY);
 }
+
 function upgrade() {
     if (pets >= multcost) {
         pets -= multcost;
@@ -25,9 +29,9 @@ function upgrade() {
         update_html();
     }
 }
+
 function reset() {
     localStorage.clear('multiplier','multcost','pets');
-
     pets = parseInt(localStorage['pets']) || 0;
     multcost = parseInt(localStorage['multcost']) || 100;
     multiplier = parseInt(localStorage['multiplier']) || 1;
@@ -38,7 +42,7 @@ function reset() {
 function update_html() {
     document.getElementById("upgrade-price").innerHTML = "Upgrade Price: " + multcost;
     document.getElementById("amount").innerHTML = pets;
-    if (multiplier > totalImages) {document.getElementById("pet-button").src = String("GnocchiPictures/Gnocchi Image10.PNG");}
+    if (multiplier > totalImages) {document.getElementById("pet-button").src = String("GnocchiPictures/Gnocchi Image" + parseInt(Math.random() * (totalImages - 1) + 1) + ".PNG");}
     else {document.getElementById("pet-button").src = String("GnocchiPictures/Gnocchi Image" + multiplier + ".PNG");}
     document.getElementById("level").innerHTML = String("Level " + multiplier);
 }
@@ -53,4 +57,34 @@ function update_version() {
     if (Math.floor(currentVersion) - Math.floor(previouslyOpenedVersion) != 0) {reset()}
     previouslyOpenedVersion = currentVersion;
     localStorage['version'] = previouslyOpenedVersion;
+}
+
+function purr_effect(xpos, ypos) {
+    console.log("purr function ran");
+    console.log(xpos + ", " + ypos);
+    let purr = document.createElement('div');
+    purr.setAttribute("class", "purr-effect");
+    purr.innerHTML = set_effect_text();
+    document.getElementById("effects").appendChild(purr);
+    purr.style.left = ((xpos + Math.floor(Math.random()*100-50)) + "px");
+    purr.style.top = ((ypos + Math.floor(Math.random()*100-50)) + "px");
+    setTimeout(() => {purr.remove();}, 1000);
+}
+
+function set_effect_text() {
+    text = "";
+    if (Math.floor(Math.random()*2)) {
+        text = "pur"
+        for (i=0; i<(Math.random()*5); i++) {
+            text += "r"
+        }
+    }
+    else {
+        text = "me"
+        for (i=0; i<(Math.random()*5); i++) {
+            text += "o"
+        }
+        text += "w"
+    }
+    return text
 }
